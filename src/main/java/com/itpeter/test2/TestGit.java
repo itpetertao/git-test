0a38e430d35ba0e47186a9073b1421d72a7cf60b
peter <peter@itpeter.com>
2023-03-12 02:40:23 +0800

    first commit

---
+++ src/main/java/com/itpeter/test2/TestGit.java
@@ -0,0 +1,12 @@
+package com.itpeter.test2;
+
+/**
+ * @author 黄榕涛
+ * @version 1.0
+ * @create 2023/3/12 2:39
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("this is a new git test");
+    }
+}
